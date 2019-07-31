cf10d833189bdd69d5bfcdb6bb296011d673077e
Tawhid Ali <tawhidali126@gmail.com>
2019-07-31 15:37:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,4 +13,24 @@ end
 def pop_with_args(array)
   array.pop(2)
 end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array[3].insert(element)
+end
+
+
+
+
   
